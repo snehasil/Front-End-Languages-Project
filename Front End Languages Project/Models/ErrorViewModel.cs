a825c03353ba00ec32366c9a794ac14374c7d834
Sneha Sil <snehasil413@gmail.com>
2021-06-16 09:08:41 -0500

    created project

---
+++ Front End Languages Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Front_End_Languages_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
