1ebe4ca3f7ebe87845a6922e621eaaecd26248c9
Sneha Sil <snehasil413@gmail.com>
2021-06-17 11:19:05 -0500

    Javascript show/hide

---
+++ Front End Languages Project/HTML/ourSite.js
@@ -0,0 +1,9 @@
+﻿function showHide() {
+    var x = document.getElementById("vision");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
